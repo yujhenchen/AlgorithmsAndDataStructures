4d65cf2aaf1d2d162eeae7f0bf6367fe1b54c907
yujhenchen <yujhen.chen93@gmail.com>
2021-11-08 07:36:37 +0800

    add sumzero solution my

--- multiplePointers/sumZero_my.py
+++ multiplePointers/sumZero_my.py
@@ -1,6 +1,18 @@
-
-def sumZero(array):
+def sumZero(array1):
     # Loop element in the array
     # Use 0 to minus the current element and get target element
     # if the target element in the array, return the element and the target element
     # else return None
+    for i in range(len(array1)):
+        target = 0 - array1[i]
+        if target not in array1:
+            return None
+        if i == array1.index(target):
+            return None
+        return [array1[i], target]
+
+
+print(sumZero([-3, -2, -1, 0, 1, 2, 3]))
+print(sumZero([-2, 0, 1, 3]))
+print(sumZero([1, 2, 3]))
+print(sumZero([0]))
