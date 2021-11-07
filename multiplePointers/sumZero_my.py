741d689c8b5c72b8b8f47f824b0bd5761742fa83
yujhenchen <yujhen.chen93@gmail.com>
2021-11-08 07:26:00 +0800

    add sumZero desc

---
+++ multiplePointers/sumZero_my.py
@@ -0,0 +1,6 @@
+
+def sumZero(array):
+    # Loop element in the array
+    # Use 0 to minus the current element and get target element
+    # if the target element in the array, return the element and the target element
+    # else return None
