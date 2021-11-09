87a9611f7ae3fb73d2a5e0c8c6e5fa9c3848cbe0
yujhenchen <yujhen.chen93@gmail.com>
2021-11-09 20:21:12 +0800

    fix sum zero move incorrect

--- multiplePointers/sum_zero_refactor.py
+++ multiplePointers/sum_zero_refactor.py
@@ -1,2 +1,29 @@
-def sum_zero():
-    return
+def sum_zero(array1):
+    # Loop element from left of array and from the right of array
+    # If right < left, return None
+    # else if element at index left + element at index right == 0, return array1[left] and  array1[right]
+    # else return None
+    left = 0
+    right = len(array1) - 1
+    sumNum = 0
+    while right >= left:
+        #print(array1[left])
+        #print(array1[right])
+        sumNum = array1[left] + array1[right]
+        if sumNum == 0:
+            if left == right:
+                return [array1[left]]
+            else:
+                return [array1[left], array1[right]]
+        elif sumNum>0:
+            right = right - 1
+        else:
+            left = left + 1
+
+    return None
+
+
+print(sum_zero([-3, -2, -1, 0, 1, 2, 3]))
+print(sum_zero([-2, 0, 1, 3]))
+print(sum_zero([1, 2, 3]))
+print(sum_zero([0]))
