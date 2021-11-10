d85234e8954c1fe2d33e972b10a35fd2a2985bb1
yujhenchen <yujhen.chen93@gmail.com>
2021-11-11 06:11:37 +0800

    add factorial recursive version

--- recursive/factorial.py
+++ recursive/factorial.py
@@ -1,7 +1,15 @@
+# not recursive solution
+# def factorial(num):
+#     total = 1
+#     for i in range(num,1,-1):
+#         total *= i
+#     return total
+
+# Recursive solution
 def factorial(num):
-    total = 1
-    for i in range(num,1,-1):
-        total *= i
-    return total
+    if num == 1:
+        return num
+    return num * factorial(num - 1)
+
 
 print(factorial(4))
