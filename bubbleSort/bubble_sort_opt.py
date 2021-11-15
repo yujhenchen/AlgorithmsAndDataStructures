bd017c6fd5a6f4c50efe586a36c2abf84a64cbb4
yujhenchen <yujhen.chen93@gmail.com>
2021-11-16 07:49:26 +0800

    add opt bubble sort

--- bubbleSort/bubble_sort_opt.py
+++ bubbleSort/bubble_sort_opt.py
@@ -1,5 +1,18 @@
 def bubble_sort(arr):
+    # use a boolean in inner loop to control whether swap or not
+    no_swap = True
+    tmp = 0
+    for i in range(len(arr), 0, -1):
+        for j in range(i-1):
+            if arr[j] > arr[j+1]:
+                no_swap = False
+                tmp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = tmp
+        if no_swap:
+            break
     return arr
+
 
 print(bubble_sort([1, 3, 2]))
 print(bubble_sort([4, 3, 1, 2]))
