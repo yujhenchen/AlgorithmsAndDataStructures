d130bc25a443c9ab62b35e9721525d8f2fc3385a
yujhenchen <yujhen.chen93@gmail.com>
2021-11-16 22:07:10 +0800

    add insert sort algo my

---
+++ insertionSort/insertion_sort_my.py
@@ -0,0 +1,6 @@
+def insertion_sort(arr):
+    # loop the array current element curr_element
+    # loop the pre array
+    # compare each element in pre array
+    # set curr_element to be next elemnt of element in pre array if curr_element > element in pre array
+    return arr
