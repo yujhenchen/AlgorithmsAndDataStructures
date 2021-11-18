23b083011f5f192579dc7b22bcffe0993f8388d6
yujhenchen <yujhen.chen93@gmail.com>
2021-11-18 20:39:56 +0800

    add insert sort first attempt

--- insertionSort/insertion_sort_my.py
+++ insertionSort/insertion_sort_my.py
@@ -3,4 +3,21 @@ def insertion_sort(arr):
     # loop the pre array
     # compare each element in pre array
     # set curr_element to be next elemnt of element in pre array if curr_element > element in pre array
-    return arr
+    sorted_ls = [arr[0]]
+    curr_i = len(sorted_ls) - 1
+    for i in range(1, len(arr)):
+        while curr_i >= 0:
+            if sorted_ls[curr_i] < arr[i]:
+                sorted_ls.insert(curr_i + 1, arr[i])
+                break
+            else:
+                if curr_i == 0:
+                    sorted_ls.insert(0, arr[i])
+                    break
+            curr_i -= 1
+    return sorted_ls
+
+
+print(insertion_sort([1, 3, 2]))
+print(insertion_sort([4, 3, 1, 2]))
+print(insertion_sort([1, 2, 4, 3, 8, 9, 7, 11, 33, 12]))
