d9cb68907bcff476657f95667449dec438c37285
yujhenchen <yujhen.chen93@gmail.com>
2021-11-22 07:43:01 +0800

    add merging sort function

--- mergeSort/merge_sort_my.py
+++ mergeSort/merge_sort_my.py
@@ -4,7 +4,23 @@ def merging_arrays(arr1, arr2):
     # push the smaller one into arr
     # push the rest elemtn in another array if one array arrives to the end
     arr = []
+    curr_1 = 0
+    curr_2 = 0
+    while curr_1 < len(arr1) and curr_2 < len(arr2):
+        arr.append(min(arr1[curr_1], arr2[curr_2]))
+        arr.append(max(arr1[curr_1], arr2[curr_2]))
+        curr_1 += 1
+        curr_2 += 1
+
+    if len(arr1) > len(arr2):
+        arr.extend(arr1[curr_1:])
+    else:
+        arr.extend(arr2[curr_2:])
     return arr
+
 
 def merge_sort(arr):
     return
+
+
+print(merging_arrays([1, 10, 50], [2, 14, 90, 100]))
