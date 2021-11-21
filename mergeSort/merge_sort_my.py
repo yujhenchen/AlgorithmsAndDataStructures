874aa79152f383a37e9da83dcf4f46c40674fff0
yujhenchen <yujhen.chen93@gmail.com>
2021-11-22 07:31:00 +0800

    add merging array steps

--- mergeSort/merge_sort_my.py
+++ mergeSort/merge_sort_my.py
@@ -1,5 +1,8 @@
 def merging_arrays(arr1, arr2):
-    # Merged the 2 arrays and create another array that is also sorted
+    # merged the 2 arrays and create another array that is also sorted
+    # walk through elements in both array and comoare
+    # push the smaller one into arr
+    # push the rest elemtn in another array if one array arrives to the end
     arr = []
     return arr
 
