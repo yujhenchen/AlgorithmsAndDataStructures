42ee1699da70fc4515361929bc58bd67bbee0718
yujhenchen <yujhen.chen93@gmail.com>
2021-11-25 22:06:31 +0800

    add doubly linked list structure

---
+++ doublyLinkedList/linked_list.py
@@ -0,0 +1,35 @@
+class Node(object):
+    def __init__(self, value):
+        super().__init__()
+        self.next = None
+        self.prev = None
+        self.value = value
+
+
+class DoublyLinkedList(object):
+    def __init__(self):
+        super().__init__()
+
+    def push(self, value):
+        return
+
+    def pop(self):
+        return
+
+    def shift(self):
+        return
+
+    def unshift(self, value):
+        return
+
+    def get(self, index):
+        return
+
+    def set(self, index, value):
+        return
+
+    def insert(self, index, value):
+        return
+
+    def remove(self, index):
+        return
