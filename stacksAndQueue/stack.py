f4defde14e7f54ad743b120e6bef35f4de52d675
yujhenchen <yujhen.chen93@gmail.com>
2021-11-30 07:31:32 +0800

    stack add my work

--- stacksAndQueue/stack.py
+++ stacksAndQueue/stack.py
@@ -1,2 +1,22 @@
 # last in first out
 # where stack is used? 1. managing function invocations, undo redo, routing (the history objects)
+# implement stack using array
+
+
+class Stack(object):
+    def __init__(self):
+        super().__init__()
+        self.data = []
+        self.length = 0
+
+    def push(self, value):
+        self.data.append(value)
+        self.length += 1
+        return
+
+    def pop(self):
+        if self.length < 1:
+            return None
+        self.length -= 1
+        value = self.data.pop()
+        return value
