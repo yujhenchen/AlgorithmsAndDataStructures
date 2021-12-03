50210f0b68dbb7ce06fba53c48be6b048bee3846
yujhenchen <yujhen.chen93@gmail.com>
2021-12-03 08:01:16 +0800

    bst add test find method

--- binarySearchTree/test_bts.py
+++ binarySearchTree/test_bts.py
@@ -1,7 +1,8 @@
 import pytest
 from bst import BST
 
-# @pytest.mark.skip(reason="wait for finishing")
+
+@pytest.mark.skip(reason="wait for finishing")
 def test_insert():
     bst = BST()
     bst.insert(1)
@@ -13,8 +14,18 @@ def test_insert():
     bst.insert(14)
     return bst
 
-@pytest.mark.skip(reason="wait for finishing")
+
+#@pytest.mark.skip(reason="wait for finishing")
 def test_search():
-    bst = test_insert
-    bst.find(9)
-    bst.find(19)
+    bst = test_insert()
+    found_9 = bst.find(9)
+    if found_9 is not None:
+        print(found_9.value)
+    else:
+        print("cannot find 9")
+
+    found_13 = bst.find(13)
+    if found_13 is not None:
+        print(found_13.value)
+    else:
+        print("cannot find 13")
