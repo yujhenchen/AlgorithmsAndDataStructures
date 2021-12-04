16f3c94694d8beafea138f9893fdb4df50173363
yujhenchen <yujhen.chen93@gmail.com>
2021-12-05 06:27:17 +0800

    add bfs desc

---
+++ treeTraversal/bfs.py
@@ -0,0 +1,6 @@
+# add root in the queue
+# if there's anything in the queue, add into visited queue
+# if the first node in the queue has left, move left into queue
+# if the first node in the queue has right, move right into queue
+# everytime set current root to the first node in the queue
+# return visited queue when current root is None
