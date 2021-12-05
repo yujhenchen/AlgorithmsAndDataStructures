8fa225990143f6d6276298d25909661bf190255c
yujhenchen <yujhen.chen93@gmail.com>
2021-12-05 17:35:28 +0800

    dfs post order add debug use print each recursion

--- treeTraversal/dfs.py
+++ treeTraversal/dfs.py
@@ -25,4 +25,11 @@ def post_order_dfs(root):
 
     if root is None:
         return visited
-    post_order_dfs(root.left)
+
+    post_order_dfs(root.left), post_order_dfs(root.right)
+
+    if root.left is not None:
+        print("root.left.value: " + str(root.left.value))
+    if root.right is not None:
+        print("root.right.value: " + str(root.right.value))
+
