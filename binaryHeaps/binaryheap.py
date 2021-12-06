3f0bdc26cff23492fb2fe3f5fb981983cef7c525
yujhenchen <yujhen.chen93@gmail.com>
2021-12-06 22:33:56 +0800

    binary heaps add desc

---
+++ binaryHeaps/binaryheap.py
@@ -0,0 +1,4 @@
+# there are maximum two nodes of root node
+# the left and right node have no order
+# MaxBinaryHeap: parent nodes are always larger than child nodes
+# MinBinaryHeap: parent nodes are always smaller than child nodes
