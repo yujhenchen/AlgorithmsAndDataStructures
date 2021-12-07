5f4f456e53b5894e866f7336f561bdf28b12a733
yujhenchen <yujhen.chen93@gmail.com>
2021-12-08 07:04:08 +0800

    binaryheap add sort desc

--- binaryHeaps/binaryheap.py
+++ binaryHeaps/binaryheap.py
@@ -5,3 +5,7 @@
 
 # MaxBinaryHeap: parent nodes are always larger than child nodes
 # MinBinaryHeap: parent nodes are always smaller than child nodes
+
+# Sorting Heaps:
+# Has parent to find child: For any index of an array n, the left child is sorted at 2n+1, the right child is at 2n+2
+# Has child to find parent: For any child node at index n, Its parent is at index floor(( n-1)/2 )
