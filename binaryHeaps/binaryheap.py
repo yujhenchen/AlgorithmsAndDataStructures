5cae45839c87e485eef09d341464c6dc0d7d40da
yujhenchen <yujhen.chen93@gmail.com>
2021-12-08 08:08:04 +0800

    binaryheap add insert first attempt my

--- binaryHeaps/binaryheap.py
+++ binaryHeaps/binaryheap.py
@@ -10,13 +10,43 @@
 # Has parent to find child: For any index of an array n, the left child is sorted at 2n+1, the right child is at 2n+2
 # Has child to find parent: For any child node at index n, Its parent is at index floor(( n-1)/2 )
 
+import math
+
+
+class Node(object):
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+        self.left = None
+        self.right = None
+
 
 class MaxBinaryHeap(object):
     def __init__(self):
         super().__init__()
         self.values = []
 
-    def insert(self):
+    def insert(self, value):
+        # add to the end
+        # bubble up: take the newly inserted node to compare with parent node and swap if it is greater than parent node
+        # and keep comparing and swaping until it reach to the root
+        # find the parent of the node: use the formula and the nodes that are in values array
+        node = Node(value)
+
+        if self.values.count == 0:
+            self.values.append(node)
+        else:
+            nodeIndex = self.values.count - 1
+            parentNodeIndex = math.floor((nodeIndex - 1) / 2)
+            parentNode = self.values[parentNodeIndex]
+            while node.value > parentNode.value:
+                self.values[nodeIndex] = parentNode
+                self.values[parentNodeIndex] = node
+                # bubble up
+                nodeIndex = parentNodeIndex
+                parentNodeIndex = math.floor((nodeIndex - 1) / 2)
+                parentNode = self.values[parentNodeIndex]
+
         return
 
     def extractMax(self):
