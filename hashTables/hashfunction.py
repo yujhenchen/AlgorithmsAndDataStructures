c8f933c72f8a8488d284826e3df67ca0b0ef1cfc
yujhenchen <yujhen.chen93@gmail.com>
2021-12-13 19:05:11 +0800

    hash tables add desc of hash function

--- hashTables/hashfunction.py
+++ hashTables/hashfunction.py
@@ -1,2 +1,7 @@
 # In order to look up values by key, we need a way to convert keys into valid indices
 # A function that performs this task is called a hash function
+
+# What makes a good hash?
+# 1. Fase (i.e. constant time)
+# 2. Don't cluster outputs at specific indices. but distributes uniformly
+# 3. Deterministic (same input yields same output)
