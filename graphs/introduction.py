fe2f2bd4f857232b32dbbd4df602f12e5ef01b20
yujhenchen <yujhen.chen93@gmail.com>
2022-01-03 10:51:34 +0800

    graphs add desc

--- graphs/introduction.py
+++ graphs/introduction.py
@@ -1,1 +1,11 @@
 # Where to use graphs: 
+# Social networks, location, routing system, visual hierarchy, file system optimizations
+
+# Essential graph terms:
+# Vertex: a node
+# Edge: a connection between nodes
+# Weighted/ Unweighted: values assigned to distances between vertices
+# Directed/ Undirected: directions assigned to distanced between vertices
+
+# Type of graphs:
+# Undirected graph, directed graph, unweighted graph, weighted graph
