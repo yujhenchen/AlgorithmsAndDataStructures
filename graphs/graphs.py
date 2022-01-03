a972542bbfad43767577959d6624da2406039d36
yujhenchen <yujhen.chen93@gmail.com>
2022-01-03 15:49:05 +0800

    add graph methods

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -1,4 +1,4 @@
-# Where to use graphs: 
+# Where to use graphs:
 # Social networks, location, routing system, visual hierarchy, file system optimizations
 
 # Essential graph terms:
@@ -11,3 +11,21 @@
 # Undirected graph, directed graph, unweighted graph, weighted graph
 
 # Storing graphs: adjacency matrix, adjacency list
+
+
+class Graph(object):
+    def __init__(self):
+        super().__init__()
+        self.adjancencyList = {}
+
+    def addVertex(self):
+        return
+
+    def addEdge(self):
+        return
+
+    def removeEdge(self):
+        return
+
+    def removeVertex(self):
+        return
