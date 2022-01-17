427f1e5593557eef690dd7be17bf7de7cbfeb087
yujhenchen <yujhen.chen93@gmail.com>
2022-01-18 07:51:21 +0800

    graph traversal add basic files

---
+++ graphTraversal/dfs.py
@@ -0,0 +1,8 @@
+# Uses: peer to peer networking, web crawlers, finding closest matches or recommendations, shortest path
+def dfs(vertex):
+    result = []
+    visited = {}
+    
+    def dfs_helper(vertex):
+        return
+    return result
---
+++ graphTraversal/graphs.py
@@ -0,0 +1,39 @@
+# Where to use graphs:
+# Social networks, location, routing system, visual hierarchy, file system optimizations
+
+# Essential graph terms:
+# Vertex: a node
+# Edge: a connection between nodes
+# Weighted/ Unweighted: values assigned to distances between vertices
+# Directed/ Undirected: directions assigned to distanced between vertices
+
+# Type of graphs:
+# Undirected graph, directed graph, unweighted graph, weighted graph
+
+# Storing graphs: adjacency matrix, adjacency list
+
+
+class Graph(object):
+    def __init__(self):
+        super().__init__()
+        self.adjancencyList = {}
+
+    def addVertex(self, vertex):
+        if vertex not in self.adjancencyList:
+            self.adjancencyList[vertex] = []
+
+    def addEdge(self, vertex1, vertex2):
+        self.adjancencyList[vertex1].append(vertex2)
+        self.adjancencyList[vertex2].append(vertex1)
+
+    def removeEdge(self, vertex1, vertex2):
+        if vertex1 in self.adjancencyList:
+            self.adjancencyList[vertex1].remove(vertex2)
+        if vertex2 in self.adjancencyList:
+            self.adjancencyList[vertex2].remove(vertex1)
+
+    def removeVertex(self, vertex):
+        for key in self.adjancencyList:
+            if vertex in self.adjancencyList[key]:
+                self.adjancencyList[key].remove(vertex)
+        del self.adjancencyList[vertex]
